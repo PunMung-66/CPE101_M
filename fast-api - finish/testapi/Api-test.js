58bfea0f63fcf5f6c0f2eb4fb292f26babb73e00
PUN <punnawat.mungkalarungsi@gmail.com>
2023-12-24 16:52:48 +0700

    Refactor API test code and add new functions for login, add pocket, and read pocket

--- fast-api - finish/testapi/Api-test.js
+++ fast-api - finish/testapi/Api-test.js
@@ -1,26 +1,42 @@
 const axios = require('axios');
-
-// Replace 'http://your-api-url' with the actual URL of your FastAPI application
 const apiUrl = 'https://punnawat01-cpe101-fast-api.hf.space';
-const name = 'Pun_o_o';
-// Example data for testing
+
+// Testing the login endpoint
 const loginData = {
-    name: 'Ben',
+    name: 'Kamil',
     bank_u: 'Genshin',
     bank_p: 'Primogem',
 };
-
+const login = async () => {
+    try {
+    const response = await axios.post(`${apiUrl}/login`, loginData);
+    console.log('Login Result:', response.data.result);
+  } catch (error) {
+    console.error('Login Error:', error);
+  }
+}
+//Testing add_pocket endpoint
 const pocketData = {
-    Pocket: 'TestPocket',
-    Amount: 100,
+        Pocket: 'Test1',
+        Amount: 100,
 };
 
-// Testing the login endpoint
-// axios.post(`${apiUrl}/login`, loginData).then(response => {
-//     console.log('Login Result:', response.data.result);
-// }).catch(error => console.error('Login Error:', error));
-    
-// Testing add_pocket endpoint
-// axios.post(`${apiUrl}/add_pocket/${name}`, pocketData)
-// .then(pocketResponse => {console.log('Add Pocket Result:', pocketResponse.data.result);}).catch(error => console.error('Add Pocket Error:', error));
-    
+const addPocket = async () => {
+    try {
+    const name = 'Pun_o_o'; // Replace with the desired username
+    const response = await axios.post(`${apiUrl}/add_pocket/${name}`, pocketData);
+    console.log('Add Pocket Result:', response.data.result);
+  } catch (error) {
+    console.error('Add Pocket Error:', error);
+  }
+};
+
+const readPocket = async () => {
+    try {
+    const name = 'Pun_o_o'; // Replace with the desired username
+    const response = await axios.get(`${apiUrl}/read_pocket/${name}`);
+    console.log('Read Pocket Result:', response.data.result);
+  } catch (error) {
+    console.error('Read Pocket Error:', error);
+  }
+};
